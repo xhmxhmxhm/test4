83087dbd786c94a998ae872e90ba3b79a1ba77e6
xhmxhmxhm <1173131411@qq.com>
2019-03-18 21:54:57 +0800

    test5

---
+++ testt/src/testt/student.java
@@ -0,0 +1,16 @@
+package testt;
+
+public class student {
+
+	//private Object System;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+
+
+
+
+	}
+
+}
